3e8cb58e316ef064a5a4fe4bc96969ad396633e1
EzraMarin <90418312+EzraMarin@users.noreply.github.com>
2022-01-11 23:03:02 -0700

    Add project files.

---
+++ Mission2Assignment/wwwroot/js/grade.js
@@ -0,0 +1,11 @@
+﻿$("#btnSend").click(function () {
+    alert("asssignments: " + $("#assignments").val() +
+        ", group: " + $("#group").val() +
+        ", quizzes: " + $("#quizzes").val() +
+        ", exams: " + $("#exams").val() +
+        ", intex: " + $("#intex").val());
+})
+
+
+
+
